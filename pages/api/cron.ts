11a9aedb0cf3471e6f1c0882201a0d3637e9e586
Gonza Rafuls <grafuls@redhat.com>
2023-05-06 10:57:05 +0200

    fix: price parse

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -17,7 +17,7 @@ async function update() {
   const data = await fetch('https://www.cotodigital3.com.ar/sitios/cdigi/producto/-tomate-red-x-kg/_/A-00000684-00000684-200');
   const $ = cheerio.load(await data.text());
   const element = $('.atg_store_newPrice').first()
-  const tomatoPrice = element.text().trim().replace(/[^0-9\.\,-]+/g,"")
+  const tomatoPrice = element.text().trim().replace(/[^0-9\.\,-]+/g,"").replace(".","")
   const timestamp = new Date().toISOString();
 
   await supabase.from('tomato_prices').insert([{ timestamp, price: parseFloat(tomatoPrice)}])
